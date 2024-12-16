8e270cf21f924b5d6cdccec3e40471921a4c4e0b
Nicole Espiritu <146156901+143nicolay@users.noreply.github.com>
2024-12-16 14:01:33 +0800

    config.php

--- app1_nicole/config.php
+++ app1_nicole/config.php
@@ -1,8 +1,8 @@
 <?php
-  define('DB_SERVER', 'localhost');
-  define('DB_USERNAME', 'root');
-  define('DB_PASSWORD', '');
-  define('DB_NAME', 'student');
+  define('DB_SERVER', "localhost");
+  define('DB_USERNAME', "nicole");
+  define('DB_PASSWORD', "1234");
+  define('DB_NAME', "student");
 
   $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
